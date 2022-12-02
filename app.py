db4b7cfeb787832af9908dd4dde6333003b78112
nandakishore <nk732100@gmail.com>
2022-12-02 20:23:02 +0530

    register logic is handled - v1

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 
+# . -> current dir
+# .. -> parent dir
 
-app = Flask(__name__, template_folder=".")
+app = Flask(__name__)
 print("__name__:", __name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p> Hello user </p>"
     return render_template("welcome.html")
-    
+
+@app.route("/login")
+def login():
+    # username: root
+    # password: groot
+
+    # redirect
+    # return render_template("welcome.html")
+
+    return render_template("Login.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        return "Congrats {}, you have registered successfully".format(username)
+
+    return render_template("Register.html")
